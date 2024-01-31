5f906e18a632ecea48714a96d9b6492b22598964
Dee-Codez <debampati44@gmail.com>
2024-01-31 06:26:12 +0530

    VercelDeploy_1

--- server/db.js
+++ server/db.js
@@ -1,11 +1,13 @@
 const Pool = require('pg').Pool;
+require('dotenv').config()
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'password',
-    host: 'localhost',
-    port: '5432',
-    database: 'lettdb',
+    connectionString:process.env.POSTGRES_URL + "?sslmode=require",
 });
+
+pool.connect((err) => {
+    if (err) throw err;
+    console.log("Connect to PostgreSQL successfully");
+})
 
 module.exports = pool;
