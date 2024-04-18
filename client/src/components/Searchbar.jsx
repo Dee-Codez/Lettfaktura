21b3bcffcbf6a65ed2af516703ddce6378c46cff
Dee-Codez <debampati44@gmail.com>
2024-04-18 07:01:46 +0530

    PDF Invoicing_Integrated

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import {useState} from 'react'
 import { IoMdAdd, IoMdSearch} from 'react-icons/io'
 import { FaPrint } from "react-icons/fa6";
 import { LuSettings2 } from "react-icons/lu";
 import {Link, useNavigate} from 'react-router-dom'
-
+import { jsPDF } from "jspdf";
+import autoTable from "jspdf-autotable";
 
 const Searchbar = ({searchATerm, setsearchATerm, searchPTerm, setsearchPTerm}) => {
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const generatePDF = async () => {
+        setLoading(true);
+        const doc = new jsPDF();
+
+        doc.setFontSize(20);
+        doc.text("Dee Codez", 15, 15);
+        doc.setFontSize(10);
+        doc.text("W-306, Abode Valley, Potheri", 15, 20);
+        doc.text("Chennai, TN, 603203", 15, 25);
+        doc.text("+91 9454315004", 15, 30);
+
+
+        const logo = await fetch('https://avatars.githubusercontent.com/u/114132607?v=4').then(response => response.blob());
+        const reader = new FileReader();
+        reader.readAsDataURL(logo);
+        reader.onloadend = function() {
+            const base64logo = reader.result;                
+            doc.addImage(base64logo, 'JPEG', 150, 5, 30, 30);
+        }
+
+        doc.setFontSize(25);
+        const pageWidth = doc.internal.pageSize.getWidth();
+        doc.text("INVOICE", pageWidth / 2, 65, { align: 'center' });
+
+        doc.setFontSize(10);
+        const date = new Date();
+        doc.text("Order Date: " + date.toLocaleDateString(), 15, 75);
+        doc.text("Order Time: " + date.toLocaleTimeString(), 15, 80);
+
+        const tableColumn = ["Article_ID", "Title", "Price", "Unit", "Stock", "Description"];
+        const tableRows = [];
+
+        const products = await fetch('https://lettfaktura-backend.vercel.app/articles').then(response => response.json());
+
+        products.forEach(product => {
+            const productData = [product.article_id, product.title, product.price, product.unit, product.stock, product.descr];
+            tableRows.push(productData);
+        });
+
+        doc.autoTable(tableColumn, tableRows, { startY: 90 });
+
+        doc.setFontSize(10);
+        const pageHeight = doc.internal.pageSize.getHeight();
+        doc.text("This is an Auto-Generated PDF", pageWidth / 2, pageHeight - 20,{ align: 'center'});
+        doc.text("For Personal Use, Head On To lettfaktura.vercel.app And Check It Out", pageWidth / 2, pageHeight - 15,{ align: 'center'});
+        doc.text("Created By Dee Codez!", pageWidth / 2, pageHeight - 9,{ align: 'center'});
+
+        const timestamp = Date.now();
+        doc.save(`Invoice_${timestamp}.pdf`);
+        setLoading(false);
+    }
   return (
     <div className='flex gap-2 md:gap-5 w-full mt-5 justify-center items-center '>
         <div className='flex flex-col px-4 w-full md:flex-row flex-wrap items-center justify-center gap-10 lg:gap-20'>
@@ -42,10 +95,17 @@ const Searchbar = ({searchATerm, setsearchATerm, searchPTerm, setsearchPTerm}) =
                     </Link>
                 </div>
                 <div className='rounded-3xl h-fit p-2 w-fit flex flex-row text-black justify-center items-center border hover:bg-slate-100 shadow-md hover:shadow-none border-slate-400'>
-                    <Link to={'print'} className='flex flex-row flex-wrap gap-1 px-6 2xl:px-2 items-center md:w-50 h-full transition ease-in-out duration-300'>
+                    {loading ? <div className='flex justify-center gap-2 m-1 items-center text-white'>
+                        Loading..
+                        <div className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+                        </div>
+                    </div> : 
+                    <Link to={'print'} onClick={generatePDF} className='flex flex-row flex-wrap gap-1 px-6 2xl:px-2 items-center md:w-50 h-full transition ease-in-out duration-300'>
                         <div className='hidden 2xl:flex text-black pr-1'>Print List</div>
                         <FaPrint fontSize={30} color='yellow'/>
                     </Link>
+                    }
+                    
                 </div>
                 <div className='rounded-3xl h-fit p-2 w-fit flex flex-row text-black justify-center items-center border hover:bg-slate-100 shadow-md hover:shadow-none border-slate-400'>
                     <Link to={'advanced'} className='flex flex-row flex-wrap gap-1 px-6 2xl:px-2 items-center md:w-50 h-full transition ease-in-out duration-300'>
